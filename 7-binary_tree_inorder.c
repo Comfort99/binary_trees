76198e9c652e4f10e3b46656e1e129e81c1828b3
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 00:48:48 +0100

    alx

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,12 +6,12 @@
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!true || !func)
-    {
-        return;
-    }
+	if (!tree || !func)
+	{
+		return;
+	}
 
-    binary_tree_inorder(tree->left);
-    func(tree->data);
-    binary_tree_inorder(tree->right);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
