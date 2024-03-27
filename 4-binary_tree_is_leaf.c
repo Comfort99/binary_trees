e5d4cb09798cc7086613dfdee501a5c98a4097c3
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 00:38:33 +0100

    alx

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
-  *
-  *
-  *
-  *
+  *  binary_tree_is_leaf - This the function. M_I_N_E
+  * @node: This is ptr to tree
+  * Return: This returns an integer
   */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
@@ -11,6 +10,5 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 		return (0);
 	if (!node->left && !node->right)
 		return (1);
-	else
-			return (0);
+	return (0);
 }
