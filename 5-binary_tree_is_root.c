9c792d10581b38becd3b13a00e4a0e92d14223fe
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 00:43:43 +0100

    alx

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,17 +1,14 @@
 #include "binary_trees.h"
 /**
-  *
-  *
-  *
-  *
-  *
+  * binary_tree_is_root - This is the func
+  * @node: This is the node ptr
+  * Return: This returns am int
   */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-		if(!node)
-				return(0);
+		if (!node)
+			return (0);
 		if (!node->parent)
-				return(1);
-		else
-				return(0);
+			return (1);
+		return (0);
 }
