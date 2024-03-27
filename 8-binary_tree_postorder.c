e26d9d8b501ef4c0883850a71e3835bbd903b039
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 00:51:35 +0100

    alx

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
-  *
-  *
-  *
-  *
+  *  binary_tree_postorder - This is the func
+  * @tree: This is the tree ptr
+  * @func: This is the function ptr
   */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
@@ -12,5 +11,4 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	(*func)(tree->n);
-	return;
 }
