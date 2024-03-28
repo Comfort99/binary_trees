81f3751c5755f3251bc9a388e90973787c6abee1
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 03:59:51 +0100

    alx

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,13 @@
   */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-		size_t result;
+		size_t result, left, right;
 
 			if (!tree || (!tree->left && !tree->right))
 				return (0);
-		size_t left = binary_tree_height(tree->left);
-		size_t right = binary_tree_height(tree->right);
-			if (left > right)
+		left = binary_tree_height(tree->left);
+		right = binary_tree_height(tree->right);
+		if (left > right)
 			result = left + 1;
 			else
 				result = right + 1;
