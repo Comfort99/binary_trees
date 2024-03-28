fddc023ef1839ec0f7fa639374783b2872ebe7bd
MercyAkpan <akpanmeme@gmail.com>
2024-03-28 02:45:03 +0100

    alx

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+  *
+  *
+  *
+  *
+  *
+  */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+		size_t depth = 0;
+
+		if (!tree)
+			return(depth);
+		while (tree->parent)
+			{
+					depth += 1;
+					tree = tree->parent;
+			}
+		return (depth);
+}
