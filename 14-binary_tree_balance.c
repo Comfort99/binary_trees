0cb21a24c13e2b06a5d0676852d2003269f5a760
MercyAkpan <akpanmeme@gmail.com>
2024-03-30 18:08:17 +0100

    alx

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
-  * binary_tree_height - Checks for the height of a node in a BT.
+  * binary_tree_balance - Checks for the height of a node in a BT.
   * @tree: This is node pointer
   * Return: This returns height of node in BT.
   * BT -- Binary tree
@@ -14,27 +14,27 @@ int binary_tree_balance(const binary_tree_t *tree)
 		left = binary_height2(tree->left);
 		right = binary_height2(tree->right);
 		balance_factor = left - right;
-        return (balance_factor);
+		return (balance_factor);
 }
 
 
 /**
-  * binary_tree_height - Checks for the height of a node in a BT.
+  * binary_height2 - Checks for the height of a node in a BT.
   * @tree: This is node pointer
   * Return: This returns height of node in BT.
   * BT -- Binary tree
   */
 int binary_height2(const binary_tree_t *tree)
 {
-                size_t result, left, right;
+			size_t result, left, right;
 
-                        if (!tree)
-                        	return (0);
-                left = binary_height2(tree->left);
-                right = binary_height2(tree->right);
-                if (left > right)
-                        result = left + 1;
-                        else
-                                result = right + 1;
-                return (result);
+			if (!tree)
+				return (0);
+			left = binary_height2(tree->left);
+			right = binary_height2(tree->right);
+			if (left > right)
+				result = left + 1;
+			else
+				result = right + 1;
+			return (result);
 }
